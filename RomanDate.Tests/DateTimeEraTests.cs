cc1ed1a22d9f15c7357b363df42eb234b724346d
Dan Hulse <dannythulse@outlook.com>
2019-04-04 16:30:45 +0100

    More work on era datetime

--- RomanDate.Tests/DateTimeEraTests.cs
+++ RomanDate.Tests/DateTimeEraTests.cs
@@ -19,5 +19,83 @@ namespace RomanDate.Tests
             Assert.AreEqual(1, newDate.DateTime.Year);
             Assert.AreEqual(Eras.BC, newDate.Era);
         }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlyAddsDaysInADEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 1, 5), Eras.AD);
+
+            var newDate = date.TryAddDays(6);
+
+            Assert.AreEqual(11, newDate.DateTime.Day);
+            Assert.AreEqual(1, newDate.DateTime.Month);
+            Assert.AreEqual(1, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.AD, newDate.Era);
+        }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlySubtractsDaysInADEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 1, 5), Eras.AD);
+
+            var newDate = date.TryAddDays(-3);
+
+            Assert.AreEqual(2, newDate.DateTime.Day);
+            Assert.AreEqual(1, newDate.DateTime.Month);
+            Assert.AreEqual(1, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.AD, newDate.Era);
+        }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlyCrossesToADEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 12, 31), Eras.BC);
+
+            var newDate = date.TryAddDays(6);
+
+            Assert.AreEqual(6, newDate.DateTime.Day);
+            Assert.AreEqual(1, newDate.DateTime.Month);
+            Assert.AreEqual(1, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.AD, newDate.Era);
+        }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlyAddsDaysInBCEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 2, 11), Eras.BC);
+
+            var newDate = date.TryAddDays(12);
+
+            Assert.AreEqual(23, newDate.DateTime.Day);
+            Assert.AreEqual(2, newDate.DateTime.Month);
+            Assert.AreEqual(1, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.BC, newDate.Era);
+        }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlyCrossesYearsInBCEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 1, 1), Eras.BC);
+
+            var newDate = date.TryAddDays(-6);
+
+            Assert.AreEqual(26, newDate.DateTime.Day);
+            Assert.AreEqual(12, newDate.DateTime.Month);
+            Assert.AreEqual(2, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.BC, newDate.Era);
+        }
+
+        [TestMethod]
+        public void TryAddDays_CorrectlySubtractsDaysInBCEra()
+        {
+            var date = new DateTimeEra(new DateTime(1, 1, 11), Eras.BC);
+
+            var newDate = date.TryAddDays(-5);
+
+            Assert.AreEqual(6, newDate.DateTime.Day);
+            Assert.AreEqual(1, newDate.DateTime.Month);
+            Assert.AreEqual(1, newDate.DateTime.Year);
+            Assert.AreEqual(Eras.BC, newDate.Era);
+        }
     }
 }
--- RomanDate/DateTimeEra.cs
+++ RomanDate/DateTimeEra.cs
@@ -29,11 +29,23 @@ namespace RomanDate
 
         public DateTimeEra TryAddDays(int days)
         {
+            if (Era == Eras.AD)
+            {
+                return AddDaysAD(days);
+            }
+            else
+            {
+                return AddDaysBC(days);
+            }
+        }
+
+        private DateTimeEra AddDaysAD(int days)
+        {
             var minAD = DateTime.MinValue;
             var startBC = new DateTime(2000, 12, 31);
             var daysBetween = (DateTime - minAD).Days;
 
-            if (Era == Eras.AD && days <= 0)
+            if (days <= 0)
             {
                 if (daysBetween <= Math.Abs(days))
                 {
@@ -44,17 +56,45 @@ namespace RomanDate
                     var eraDate = new DateTime(years, newDate2.Month, newDate2.Day, newDate2.Hour, newDate2.Minute, newDate2.Second, newDate2.Millisecond);
                     return new DateTimeEra(eraDate, Eras.BC);
                 }
+            }
 
+            var newDate = DateTime.AddDays(days);
+
+            return new DateTimeEra(newDate, Eras.AD);
+        }
+
+        private DateTimeEra AddDaysBC(int days)
+        {
+            var maxBC = new DateTime(1, 12, 31);
+            var startBC = new DateTime(2000, 12, 31);
+            var daysBetween = (maxBC - DateTime).Days;
+
+            if (days >= 0)
+            {
+                if (daysBetween <= Math.Abs(days))
+                {
+                    var daysToAdd = Math.Abs(days) - daysBetween;
+                    var newDate2 = DateTime.MinValue.AddDays(daysToAdd == 0 ? 0 : (daysToAdd - 1));
+
+                    return new DateTimeEra(newDate2, Eras.AD);
+                }
+
+                var exDate = startBC.AddDays(days);
+                var years = (startBC.Year - exDate.Year);
                 var newDate = DateTime.AddDays(days);
 
-                return new DateTimeEra(newDate);
+                return new DateTimeEra(newDate, Eras.BC);
             }
-            else if (Era == Eras.BC && days >= 0)
+            else
             {
+                var exDate = startBC.AddDays(days);
+                var years = (startBC.Year - exDate.Year);
+                var newDate = DateTime.AddYears(years).AddDays(days);
 
+                return new DateTimeEra(newDate, Eras.BC);
             }
 
-            return new DateTimeEra(DateTime.AddDays(days), Era);
+            //return new DateTimeEra(DateTime.AddDays(days), Era);
         }
 
         public DateTimeEra AddWeeks(int weeks)
