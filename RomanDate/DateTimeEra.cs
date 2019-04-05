f227d050202979770228e5918a49ece06273cdd2
Dan Hulse <dannythulse@outlook.com>
2019-04-05 09:51:12 +0100

    change

--- RomanDate/DateTimeEra.cs
+++ RomanDate/DateTimeEra.cs
@@ -45,22 +45,22 @@ namespace RomanDate
             var startBC = new DateTime(2000, 12, 31);
             var daysBetween = (DateTime - minAD).Days;
 
-            if (days <= 0)
+            if (days <= 0) // if subtracting days
             {
-                if (daysBetween <= Math.Abs(days))
+                if (daysBetween <= Math.Abs(days)) // if days left before BC is less than days subtracting
                 {
                     var daysToSubtract = Math.Abs(days) - daysBetween;
                     var newDate2 = startBC.AddDays(daysToSubtract == 0 ? 0 : -(daysToSubtract - 1));
                     var years = (startBC.Year - newDate2.Year) + 1;
 
                     var eraDate = new DateTime(years, newDate2.Month, newDate2.Day, newDate2.Hour, newDate2.Minute, newDate2.Second, newDate2.Millisecond);
-                    return new DateTimeEra(eraDate, Eras.BC);
+                    return new DateTimeEra(eraDate, Eras.BC); // return date in BC era
                 }
             }
 
             var newDate = DateTime.AddDays(days);
 
-            return new DateTimeEra(newDate, Eras.AD);
+            return new DateTimeEra(newDate, Eras.AD); // Add days to existing DateTime in AD
         }
 
         private DateTimeEra AddDaysBC(int days)
@@ -69,9 +69,9 @@ namespace RomanDate
             var startBC = new DateTime(2000, 12, 31);
             var daysBetween = (maxBC - DateTime).Days;
 
-            if (days >= 0)
+            if (days >= 0) // if adding days
             {
-                if (daysBetween <= Math.Abs(days))
+                if (daysBetween <= Math.Abs(days)) // if days left before AD is less than days adding
                 {
                     var daysToAdd = Math.Abs(days) - daysBetween;
                     var newDate2 = DateTime.MinValue.AddDays(daysToAdd == 0 ? 0 : (daysToAdd - 1));
@@ -85,7 +85,7 @@ namespace RomanDate
 
                 return new DateTimeEra(newDate, Eras.BC);
             }
-            else
+            else // else subtracting days
             {
                 var exDate = startBC.AddDays(days);
                 var years = (startBC.Year - exDate.Year);
